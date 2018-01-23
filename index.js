dbbe00680945adfff209bdd0409dffb052f75943
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-23 17:17:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const currentUser = 'Homer Simpson'
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser}`
